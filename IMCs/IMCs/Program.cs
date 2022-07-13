bc700f7fcb9adae448711dede727777fe9bcb564
majufrnc <108549564+majufrnc@users.noreply.github.com>
2022-07-13 17:09:46 -0300

    menos sofrimento

---
+++ IMCs/IMCs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IMCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coloque seu peso, por favor: ");
+
+            float peso = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Coloque sua altura, por favor: ");
+
+            float altura = (float)Convert.ToDouble(Console.ReadLine());
+            float imc = (peso/(altura * altura));
+
+            Console.WriteLine("Seu peso: " + peso);
+            Console.WriteLine("Sua altura: " + altura);
+            Console.WriteLine("Seu IMC é: " + imc);
+
+            if (imc <= 18.5)
+            {
+                Console.WriteLine("Você está abaixo do peso.");
+            }
+            else if (imc > 18.5 && imc <= 25)
+            {
+                Console.WriteLine("Você está dentro do peso normal.");
+            }
+            else if (imc > 25 && imc < 30){
+                    Console.WriteLine("Você está com sobrepeso.");
+                } else if (imc >= 30){
+                    Console.WriteLine("Você está com obesidade.");
+                }
+            }
+        }
+    }
